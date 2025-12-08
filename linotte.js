6ed2512397cc0e2420c7cc1dcc163f398b99fec8
GianniPie <daniele.pia@gmail.com>
2025-12-08 15:59:54 +0100

    McDonald 8 dec h15.58
    Bouncind dice + selection without interfere with position
    selection animated
    deselecrion not working
    pieces removal animation is ok but not working

--- linotte.js
+++ linotte.js
@@ -4,40 +4,17 @@ const dd3 = document.getElementById("dd3")
 const dd4 = document.getElementById("dd4")
 const dd5 = document.getElementById("dd5")
 
+const wr1 = document.getElementById("wr1")
+const wr2 = document.getElementById("wr2")
+const wr3 = document.getElementById("wr3")
+const wr4 = document.getElementById("wr4")
+const wr5 = document.getElementById("wr5")
+
 const rollBtn = document.getElementById("rollBtn")
 const doneBtn = document.getElementById("doneBtn")
 
-const piece_a1  = document.getElementById("piece_a1");
-const piece_a2  = document.getElementById("piece_a2");
-const piece_a3  = document.getElementById("piece_a3");
-const piece_a4  = document.getElementById("piece_a4");
-const piece_a5  = document.getElementById("piece_a5");
-const piece_b1  = document.getElementById("piece_b1");
-const piece_b2  = document.getElementById("piece_b2");
-const piece_b3  = document.getElementById("piece_b3");
-const piece_b4  = document.getElementById("piece_b4");
-const piece_b5  = document.getElementById("piece_b5");
-const piece_c1  = document.getElementById("piece_c1");
-const piece_c2  = document.getElementById("piece_c2");
-const piece_c3  = document.getElementById("piece_c3");
-const piece_c4  = document.getElementById("piece_c4");
-const piece_c5  = document.getElementById("piece_c5");
-const piece_d1  = document.getElementById("piece_d1");
-const piece_d2  = document.getElementById("piece_d2");
-const piece_d3  = document.getElementById("piece_d3");
-const piece_d4  = document.getElementById("piece_d4");
-const piece_d5  = document.getElementById("piece_d5");
-const piece_e1  = document.getElementById("piece_e1");
-const piece_e2  = document.getElementById("piece_e2");
-const piece_e3  = document.getElementById("piece_e3");
-const piece_e4  = document.getElementById("piece_e4");
-const piece_e5  = document.getElementById("piece_e5");
-
-let pices = [piece_a1, piece_a2, piece_a3, piece_a4, piece_a5, piece_b1, piece_b2, piece_b3, piece_b4, piece_b5, piece_c1, piece_c2, piece_c3, piece_c4, piece_c5, piece_d1, piece_d2, piece_d3, piece_d4, piece_d5, piece_e1, piece_e2, piece_e3, piece_e4, piece_e5];
-
 let rollAnimationID;
 let stopRollID;
-
 
 const face0 = "";
 const face1 = "https://cdn.prod.website-files.com/692d60554543f993a2e48990/692db68a59e44dfdb6cc3dd5_Asset%2011.png";
@@ -84,19 +61,20 @@ function roll(){
 
 document.querySelectorAll(".piece").forEach(el => {
     el.addEventListener("click", function() {
-        elem = document.getElementById(this.id);
+        let elem = document.getElementById(this.id);
         
-        rndH = (Math.random() * 40)+30;
-        rndV = (Math.random() * 40)+30;
+        rndH = (Math.random() * 50)+25;
+        rndV = (Math.random() * 50)+25;
         elem.style.backgroundPosition = rndH + "% " + rndV + "%";
 
-
         if (elem.classList.contains("img-bounce")) {
+
             elem.classList.remove("img-bounce"); // reset
+            document.getElementById(this.parentElement.id).classList.add("img-disappear");
         } else {
             elem.classList.add("img-bounce");
+            document.getElementById(this.parentElement.id).classList.remove("img-disappear");
         }
-
     });
 });
 
@@ -110,50 +88,42 @@ rollBtn.addEventListener("click", function() {
 
 
 function rollAnimation() {
-    var rndNum1 = Math.floor(Math.random() * 4) + 1;
-    var rndNum2 = Math.floor(Math.random() * 4) + 1;
-    var rndNum3 = Math.floor(Math.random() * 4) + 1;
-    var rndNum4 = Math.floor(Math.random() * 4) + 1;
-    var rndNum5 = Math.floor(Math.random() * 4) + 1;
+    const d = 6;
+    const e = 10;
 
-    var rndDeg1 = Math.floor(Math.random() * 20) - 10;
-    var rndDeg2 = Math.floor(Math.random() * 20) - 10;
-    var rndDeg3 = Math.floor(Math.random() * 20) - 10;
-    var rndDeg4 = Math.floor(Math.random() * 20) - 10;
-    var rndDeg5 = Math.floor(Math.random() * 20) - 10;
 
-    dd1.style.webkitTransform = 'rotate(' + rndDeg1 + 'deg)' + 'translate('+ rndNum1 * 2 + 'px,' + -rndNum1 * 2 + 'px)';
-    dd2.style.webkitTransform = 'rotate(' + rndDeg2 + 'deg)' + 'translate('+ rndNum2 * 2 + 'px,' + -rndNum2 * 2 + 'px)';
-    dd3.style.webkitTransform = 'rotate(' + rndDeg3 + 'deg)' + 'translate('+ rndNum3 * 2 + 'px,' + -rndNum3 * 2 + 'px)';
-    dd4.style.webkitTransform = 'rotate(' + rndDeg4 + 'deg)' + 'translate('+ rndNum4 * 2 + 'px,' + -rndNum4 * 2 + 'px)';
-    dd5.style.webkitTransform = 'rotate(' + rndDeg5 + 'deg)' + 'translate('+ rndNum5 * 2 + 'px,' + -rndNum5 * 2 + 'px)';
+    wr1.style.transform = 'rotate(' + rndNum(-e,e) + 'deg)' + 'translate('+ rndNum(-d,d) + 'px,' + rndNum(-d,d) + 'px)';
+    wr2.style.transform = 'rotate(' + rndNum(-e,e) + 'deg)' + 'translate('+ rndNum(-d,d) + 'px,' + rndNum(-d,d) + 'px)';
+    wr3.style.transform = 'rotate(' + rndNum(-e,e) + 'deg)' + 'translate('+ rndNum(-d,d) + 'px,' + rndNum(-d,d) + 'px)';
+    wr4.style.transform = 'rotate(' + rndNum(-e,e) + 'deg)' + 'translate('+ rndNum(-d,d) + 'px,' + rndNum(-d,d) + 'px)';
+    wr5.style.transform = 'rotate(' + rndNum(-e,e) + 'deg)' + 'translate('+ rndNum(-d,d) + 'px,' + rndNum(-d,d) + 'px)';
 
-    var rndRes1 = Math.floor(Math.random() * 6) + 1;
-    var rndRes2 = Math.floor(Math.random() * 6) + 1;
-    var rndRes3 = Math.floor(Math.random() * 6) + 1;
-    var rndRes4 = Math.floor(Math.random() * 6) + 1;
-    var rndRes5 = Math.floor(Math.random() * 6) + 1;
-
-    dd1.style.backgroundImage = "url('"+ faces[rndRes1] + "')";
-    dd2.style.backgroundImage = "url('"+ faces[rndRes2] + "')";
-    dd3.style.backgroundImage = "url('"+ faces[rndRes3] + "')";
-    dd4.style.backgroundImage = "url('"+ faces[rndRes4] + "')";
-    dd5.style.backgroundImage = "url('"+ faces[rndRes5] + "')";
+    dd1.style.backgroundImage = "url('"+ faces[rndNum(1,6)] + "')";
+    dd2.style.backgroundImage = "url('"+ faces[rndNum(1,6)] + "')";
+    dd3.style.backgroundImage = "url('"+ faces[rndNum(1,6)] + "')";
+    dd4.style.backgroundImage = "url('"+ faces[rndNum(1,6)] + "')";
+    dd5.style.backgroundImage = "url('"+ faces[rndNum(1,6)] + "')";
 } 
 
 
-
-function changeBackground() {
-  const div = document.getElementById("myDiv");
-  div.style.backgroundImage = "url('https://via.placeholder.com/200/ff0000')"; // nuova immagine
+function rndNum(numFrom, numTo) {
+    var  spanNum = numTo - numFrom + 1;
+    var num = Math.floor(Math.random() * spanNum) + numFrom; // numFrom to numTo
+    if (num < numFrom) {num = numFrom}
+    if (num > numTo) {num = numTo}
+    return num;
 }
+
 
 
 function stopRoll() {
     clearInterval(rollAnimationID);
     clearInterval(stopRollID);
+//non va
+    document.querySelectorAll(".die").forEach(el => {
+        el.classList.remove("selected");
+    });
 }     
-
 
 
 
@@ -168,6 +138,17 @@ doneBtn.addEventListener("click", function() {
 
 
 
+document.querySelectorAll(".die").forEach(el => {
+    el.addEventListener("click", function() {
+        elem = document.getElementById(this.id);
+        
+        if (elem.classList.contains("selected")) {
+            elem.classList.remove("selected"); // reset
+        } else {
+            elem.classList.add("selected");
+        }
+    });
+});
 
 
 
